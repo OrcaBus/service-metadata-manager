81133a67078245001e2cd5272c65e14e720192d2
william <william.intan@unimelb.edu.au>
2025-06-06 18:02:34 +1000

    dependencies upgrade

--- infrastructure/stage/construct/lambda-migration/index.ts
+++ infrastructure/stage/construct/lambda-migration/index.ts
@@ -3,13 +3,7 @@ import { Duration } from 'aws-cdk-lib';
 import { PythonFunction, PythonFunctionProps } from '@aws-cdk/aws-lambda-python-alpha';
 import { ISecret } from 'aws-cdk-lib/aws-secretsmanager';
 import { IVpc } from 'aws-cdk-lib/aws-ec2';
-// import { TriggerFunction } from 'aws-cdk-lib/triggers';
-
-import { Provider } from 'aws-cdk-lib/custom-resources';
-import { SubnetType } from 'aws-cdk-lib/aws-ec2';
-import { CfnFunction, IFunction } from 'aws-cdk-lib/aws-lambda';
-import { CfnResource, CustomResource } from 'aws-cdk-lib';
-import CodeProperty = CfnFunction.CodeProperty;
+import { InvocationType, Trigger } from 'aws-cdk-lib/triggers';
 
 type LambdaProps = {
   /**
@@ -26,59 +20,6 @@ type LambdaProps = {
   vpc: IVpc;
 };
 
-/* ********* Replace with TriggerFunction ********* */
-type ProviderFunctionProps = {
-  /**
-   * Vpc for the function.
-   */
-  vpc: IVpc;
-  /**
-   * The provider function.
-   */
-  function: IFunction;
-  /**
-   * Properties that get defined in the template and passed to the Lambda function via `ResourceProperties`.
-   */
-  resourceProperties?: { [keys: string]: unknown };
-  /**
-   * An additional hash property that can be used to determine if the custom resource should be updated. By default,
-   * this is the s3Key of the Lambda code asset, which is derived from the asset hash. This is used to ensure that
-   * the custom resource is updated whenever the Lambda function changes, so that the function gets called again.
-   * Add a constant value here to override this behaviour.
-   */
-  additionalHash?: string;
-};
-class ProviderFunction extends Construct {
-  private readonly _function: IFunction;
-  private readonly _response: string;
-
-  constructor(scope: Construct, id: string, props: ProviderFunctionProps) {
-    super(scope, id);
-
-    this._function = props.function;
-
-    const provider = new Provider(this, 'Provider', {
-      onEventHandler: props.function,
-      vpc: props.vpc,
-      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-    });
-    const customResource = new CustomResource(this, 'CustomResource', {
-      serviceToken: provider.serviceToken,
-      properties: props.resourceProperties,
-    });
-
-    // Update the custom resource with an additional key.
-    (customResource.node.defaultChild as CfnResource).addPropertyOverride(
-      'S3Key',
-      props.additionalHash ??
-        ((this._function.node.defaultChild as CfnFunction).code as CodeProperty).s3Key
-    );
-
-    this._response = customResource.getAttString('Response');
-  }
-}
-/* ********* End ********* */
-
 export class LambdaMigrationConstruct extends Construct {
   private readonly lambda: PythonFunction;
 
@@ -93,9 +34,10 @@ export class LambdaMigrationConstruct extends Construct {
     });
     props.dbConnectionSecret.grantRead(this.lambda);
 
-    new ProviderFunction(this, 'AutoMigrateLambdaFunction', {
-      vpc: props.vpc,
-      function: this.lambda,
+    new Trigger(this, 'MigrationLambdaTrigger', {
+      handler: this.lambda,
+      timeout: Duration.minutes(5),
+      invocationType: InvocationType.REQUEST_RESPONSE,
     });
   }
 }
