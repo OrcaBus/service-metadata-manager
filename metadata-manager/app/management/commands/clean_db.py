bf12495b7d9a6e305ded1ec7ecb733cf137114f4
william <william.intan@unimelb.edu.au>
2025-08-06 16:07:21 +1000

    Update clean_db.py

--- metadata-manager/app/management/commands/clean_db.py
+++ metadata-manager/app/management/commands/clean_db.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-from app.models import Subject, Library, Sample, Individual, Project, Contact
+
 from app.tests.utils import clear_all_data
 
 
@@ -8,17 +8,6 @@ class Command(BaseCommand):
     help = "Delete all DB data"
 
     def handle(self, *args, **options):
-        p = Subject.objects.get(pk='01JAAGKRBCX0FESK9SZNHTG3CD')
-        last_record = p.history.latest()
-        previous_record = last_record.prev_record
-        delta = last_record.diff_against(previous_record)
-
-        print('helllo')
-        for change in delta.changes:
-            print('\n')
-            print("{} changed from {} to {}".format(change.field, change.old, change.new))
-            print(
-                '\n'
-            )
+        clear_all_data()
 
         print("Done")
